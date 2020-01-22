d1480db2c882408a052bbb3110e006e2948c071b
Oleg Zilberman <oleg.zilberman@mapbox.com>
2020-01-22 10:28:13 -0800

    attempting test fix

--- libdirections-onboard/src/test/java/com/mapbox/navigation/route/onboard/MainCoroutineRule.kt
+++ libdirections-onboard/src/test/java/com/mapbox/navigation/route/onboard/MainCoroutineRule.kt
@@ -3,16 +3,18 @@ package com.mapbox.navigation.route.onboard
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.TestCoroutineScope
 import kotlinx.coroutines.test.resetMain
-import kotlinx.coroutines.test.runBlockingTest
 import kotlinx.coroutines.test.setMain
 import org.junit.rules.TestWatcher
 import org.junit.runner.Description
+import org.junit.runners.model.Statement
 
 @ExperimentalCoroutinesApi
 class MainCoroutineRule(
     val testDispatcher: TestCoroutineDispatcher = TestCoroutineDispatcher()
 ) : TestWatcher() {
+    val testCoroutineScope = TestCoroutineScope(testDispatcher)
 
     override fun starting(description: Description?) {
         super.starting(description)
@@ -24,10 +26,22 @@ class MainCoroutineRule(
         Dispatchers.resetMain()
         testDispatcher.cleanupTestCoroutines()
     }
+    override fun apply(base: Statement, description: Description?) = object : Statement() {
+        @Throws(Throwable::class)
+        override fun evaluate() {
+            Dispatchers.setMain(testDispatcher)
+
+            base.evaluate()
+
+            Dispatchers.resetMain() // reset main dispatcher to the original Main dispatcher
+            testCoroutineScope.cleanupTestCoroutines()
+        }
+    }
+
+    fun runBlockingTest(block: suspend TestCoroutineScope.() -> Unit) =
+            testCoroutineScope.runBlockingTest { block() }
 }
 
 @ExperimentalCoroutinesApi
 fun MainCoroutineRule.runBlockingTest(block: suspend () -> Unit) =
-    this.testDispatcher.runBlockingTest {
-        block()
-    }
+        this.testCoroutineScope.runBlockingTest { block() }
